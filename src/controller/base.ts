c9866367cce5cb05069107c8dc140d8a692cac8d
huangwantong <huangwantong@escopetech.com>
2023-03-30 16:20:08 +0800

    chore: 完善结果集封装

--- src/controller/base.ts
+++ src/controller/base.ts
@@ -1,29 +1,14 @@
 import { Inject } from '@midwayjs/core';
-import { Controller } from '@midwayjs/decorator';
 import { Context } from '@midwayjs/web'
-
-// interface R<T = any> {
-//   code: number
-//   data: T
-//   message: string
-// }
-
-@Controller()
+import { Result, IResult } from '../utils'
 export abstract class BaseController {
   @Inject()
   ctx: Context
 
-  success<T>(data?: T, message = 'success', code = 200) {
-    return {
-      code,
-      data,
-      message
-    }
+  success<T>(data?: T, option: IResult<T> = {}): IResult<T> {
+    return Result.success<T>({ data, ...option })
   }
-  error(message = 'error', code = 900) {
-    return {
-      code,
-      message
-    }
+  error(message?, option: IResult = {}) {
+    return Result.error({ message, ...option })
   }
 }
